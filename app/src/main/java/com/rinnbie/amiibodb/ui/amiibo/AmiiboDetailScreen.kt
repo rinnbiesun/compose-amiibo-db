918fbf3800802bc5a1c866f18c062f61c074eb17
Rinnbie Sun <rinnbiesun@gmail.com>
2023-02-21 20:20:02 +0800

    Update detail screen

--- app/src/main/java/com/rinnbie/amiibodb/ui/amiibo/AmiiboDetailScreen.kt
+++ app/src/main/java/com/rinnbie/amiibodb/ui/amiibo/AmiiboDetailScreen.kt
@@ -20,36 +20,26 @@ import com.rinnbie.amiibodb.R
 import com.rinnbie.amiibodb.data.Amiibo
 import com.rinnbie.amiibodb.data.Release
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AmiiboDetailRoute(
     modifier: Modifier = Modifier,
     onNavigateBack: () -> Unit = {},
     viewModel: AmiiboDetailViewModel = hiltViewModel()
 ) {
-
     val amiiboDetailUiState: AmiiboDetailUiState by viewModel.amiiboDetailUiState.collectAsStateWithLifecycle()
 
-    Column {
-        TopAppBar(
-            title = {},
-            navigationIcon = {
-                IconButton(onClick = { onNavigateBack() }) {
-                    Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = "Back")
-                }
-            }
-        )
-        AmiiboDetailScreen(
-            modifier = modifier,
-            amiiboDetailUiState = amiiboDetailUiState
-        )
-    }
+    AmiiboDetailScreen(
+        modifier = modifier,
+        amiiboDetailUiState = amiiboDetailUiState,
+        onNavigateBack = onNavigateBack
+    )
 }
 
 @Composable
 internal fun AmiiboDetailScreen(
     modifier: Modifier,
-    amiiboDetailUiState: AmiiboDetailUiState
+    amiiboDetailUiState: AmiiboDetailUiState,
+    onNavigateBack: () -> Unit = {},
 ) {
     Column(
         modifier = modifier
@@ -62,75 +52,91 @@ internal fun AmiiboDetailScreen(
                 Text(text = "Detail Screen #")
             }
             is AmiiboDetailUiState.Success -> {
-                AmiiboDetailBody(amiibo = amiiboDetailUiState.amiibo)
+                AmiiboDetailBody(
+                    amiibo = amiiboDetailUiState.amiibo,
+                    onNavigateBack = onNavigateBack
+                )
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AmiiboDetailBody(
     modifier: Modifier = Modifier,
     amiibo: Amiibo,
+    onNavigateBack: () -> Unit = {},
     isPreview: Boolean = false,
 ) {
-    Column(
-        modifier = modifier
-            .fillMaxWidth()
-            .padding(horizontal = 16.dp),
-    ) {
-        Column(
-            modifier = modifier.fillMaxWidth(),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text(
-                text = "${amiibo.name}",
-                style = MaterialTheme.typography.titleLarge
-            )
-            Text(
-                text = "${amiibo.amiiboSeries}",
-                style = MaterialTheme.typography.titleSmall
-            )
-            Spacer(modifier = Modifier.height(24.dp))
-            if (!isPreview) {
-                AsyncImage(
-                    modifier = modifier,
-                    model = amiibo.image,
-                    error = painterResource(id = R.drawable.no_image),
-                    contentDescription = null
-                )
-            } else {
-                Image(
-                    modifier = Modifier.padding(10.dp),
-                    painter = painterResource(id = R.drawable.testing_image),
-                    contentDescription = null
-                )
+    Column {
+        CenterAlignedTopAppBar(
+            title = { Text(text = amiibo.name.orEmpty()) },
+            navigationIcon = {
+                IconButton(onClick = { onNavigateBack() }) {
+                    Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = "Back")
+                }
             }
+        )
+        Column(
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp),
+        ) {
+            Column(
+                modifier = modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = "${amiibo.amiiboSeries}",
+                    style = MaterialTheme.typography.titleSmall
+                )
+                Spacer(modifier = Modifier.height(24.dp))
+                if (!isPreview) {
+                    AsyncImage(
+                        modifier = modifier,
+                        model = amiibo.image,
+                        error = painterResource(id = R.drawable.no_image),
+                        contentDescription = null
+                    )
+                } else {
+                    Image(
+                        modifier = Modifier.padding(10.dp),
+                        painter = painterResource(id = R.drawable.testing_image),
+                        contentDescription = null
+                    )
+                }
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+            ReleaseDateSection(amiibo)
+            SeriesSection()
         }
-        Spacer(modifier = Modifier.height(16.dp))
-        ReleaseDateSection(amiibo)
     }
 }
 
 @Composable
+private fun SeriesSection() {
+
+}
+
+@Composable
 private fun ReleaseDateSection(amiibo: Amiibo) {
-    if (!amiibo.release?.au.isNullOrEmpty()) {
-        ReleaseDateItem(R.drawable.logo_australia, "au", amiibo.release?.au)
-        Spacer(modifier = Modifier.height(8.dp))
-    }
+    val releaseDateObjList = arrayListOf(
+        ReleaseDateObj(R.drawable.logo_australia, "au", amiibo.release?.au),
+        ReleaseDateObj(R.drawable.logo_europe, "eu", amiibo.release?.eu),
+        ReleaseDateObj(R.drawable.logo_japan, "jp", amiibo.release?.jp),
+        ReleaseDateObj(R.drawable.logo_north_america, "na", amiibo.release?.na),
+    )
 
-    if (!amiibo.release?.eu.isNullOrEmpty()) {
-        ReleaseDateItem(R.drawable.logo_europe, "eu", amiibo.release?.eu)
-        Spacer(modifier = Modifier.height(8.dp))
-    }
-
-    if (!amiibo.release?.jp.isNullOrEmpty()) {
-        ReleaseDateItem(R.drawable.logo_japan, "jp", amiibo.release?.jp)
-        Spacer(modifier = Modifier.height(8.dp))
-    }
-
-    if (!amiibo.release?.na.isNullOrEmpty()) {
-        ReleaseDateItem(R.drawable.logo_north_america, "na", amiibo.release?.na)
+    for ((index, releaseDateObj) in releaseDateObjList.withIndex()) {
+        ReleaseDateItem(
+            releaseDateObj.id,
+            releaseDateObj.contentDescription,
+            releaseDateObj.releaseDate
+        )
+        if (index < releaseDateObjList.size - 1) {
+            Spacer(modifier = Modifier.height(8.dp))
+        }
     }
 }
 
@@ -166,19 +172,15 @@ fun AmiiboDetailScreenPreview() {
         amiiboSeries = "大乱闘スマッシュブラザーズシリーズ",
         release = release
     )
-    Column {
-        CenterAlignedTopAppBar(
-            title = { Text(text = amiibo.name.orEmpty()) },
-            navigationIcon = {
-                IconButton(onClick = { }) {
-                    Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = "Back")
-                }
-            }
-        )
-        AmiiboDetailBody(
-            amiibo = amiibo,
-            isPreview = true
-        )
-    }
+    AmiiboDetailBody(
+        amiibo = amiibo,
+        isPreview = true
+    )
 }
+
+data class ReleaseDateObj(
+    @DrawableRes val id: Int,
+    val contentDescription: String,
+    val releaseDate: String?
+)
 
